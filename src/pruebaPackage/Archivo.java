4ee822d938a870bd239e0040de278c8d2d0bc980
gonzalo.sanz <gonzalo.sanz@AULA1-97.JUANXXIII.net>
2022-02-04 19:10:31 +0100

    Comienzo del Proyecto

---
+++ src/pruebaPackage/Archivo.java
@@ -0,0 +1,11 @@
+package pruebaPackage;
+
+public class Archivo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mensaje de Prueba");
+
+	}
+
+}
